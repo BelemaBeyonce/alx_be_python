b377686af1f15940f5481a332e0ab4893b615a8d
Belema_adim <adimbelema205@gmail.com>
2025-07-05 13:56:32 +0100

    Adding shopping list again

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -7,7 +7,7 @@ def display_current_datetime():
 
 def calculate_future_date():
     try:
-        days_to_add = int(input("Enter the number of days to add:"))
+        days_to_add = int(input("Enter the number of days to add to the current date: "))
         future_date = display_current_datetime + timedelta(days=days_to_add)
         print("Future Date:", future_date.strftime("%Y-%m-%d %H:%M:%S"))
     except ValueError:
